edfe64fc19a60dd3b6a92b59366e5c093588365f
Nobu Oyama <nobu.oyama@lightblueoptics.com>
2019-04-19 09:55:19 +0100

    Define top level layout   header-content-footer

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,66 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div id="header">
+      <p>Header</p>
     </div>
-    <router-view/>
+    <router-view id="content"/>
+    <div id="footer">
+      <p>Footer</p>
+    </div>
   </div>
 </template>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+  html, body {
+    height: 100%;
+    overflow: hidden;
+    /*background-color: magenta;*/
+  }
 </style>
+
+<style scoped>
+
+  #app {
+
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+
+    position: absolute;
+    top: 0;
+    left: 0;
+    height: 100%;
+    width: 100%;
+    margin: 0;
+    padding: 0;
+    display: flex;
+    flex-direction: column;
+    flex-wrap: nowrap;
+    justify-content: space-between;
+    align-items: center;
+    overflow: hidden;
+  }
+
+  #header {
+    padding: 10px;
+    width: 100%;
+    flex: 0 0 auto;
+    /*background-color: lime;*/
+  }
+
+  #content {
+    width: 100%;
+    padding: 10px;
+    flex: 1 1 auto;
+    /*background-color: grey;*/
+  }
+
+  #footer {
+    width: 100%;
+    padding: 10px;
+    flex: 0 0 auto;
+    /*background-color: orange;*/
+  }
+</style>
+
