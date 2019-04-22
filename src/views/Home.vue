08d9bd0384f3d98b5eb5febae52016deb1c7f028
Nobu Oyama <nobu.oyama@lightblueoptics.com>
2019-04-22 11:26:08 +0100

    Home view to show Pair view hen the system is not paired with a Kaptivo

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,3 +3,25 @@
     <h1>This is a home page</h1>
   </div>
 </template>
+
+<script>
+  import { mapGetters, mapActions } from 'vuex'
+  export default {
+    mounted() {
+      if (!this.isPaired) {
+        this.$router.push('pair');
+      }
+    },
+    computed: {
+      ...mapGetters([
+        'isPaired'
+      ]),
+    },
+    methods: {
+      ...mapActions([
+        'setMessage',
+      ]),
+    },
+  }
+</script>
+
