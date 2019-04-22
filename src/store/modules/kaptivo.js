9ba68a738904b33e095bcedbe0048c35e27efbea
Nobu Oyama <nobu.oyama@lightblueoptics.com>
2019-04-22 11:23:46 +0100

    Make pairing information persistent, and add clearRoomPairing

--- src/store/modules/kaptivo.js
+++ src/store/modules/kaptivo.js
@@ -1,77 +1,80 @@
 
+/* eslint-disable no-unused-vars */
+
 const nodeKaptivo = require('../../@lightblue/node-kaptivo/index.js');
+
+const KAPTIVO_ID_KEY = '__KAPTIVO_ID__';
+const PAIRING_TOKEN_KEY = '__PAIRING_TOKEN__';
+const SYSTEMID_KEY = '__SYSTEM_ID__';
+const CLIENT_TOKEN = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJjbGllbnQiOnsibmFtZSI6IkthcHRpdm8gTWFuYWdlciBEZXYgbW9kZSIsInJlZGlyZWN0X3VyaSI6Imh0dHA6Ly9sb2NhbGhvc3Q6ODA4Mi9pbmRleC5odG1sIiwic2NvcGUiOlsiY2FwdHVyZSIsImxvY2FsX2NhcHR1cmUiLCJwYWlyIiwiZGlzY292ZXIiLCJyZW1vdGVfY29uZmlnIiwidmlldyIsImxvY2FsX3ZpZXciLCJtb25pdG9yIiwiY29udHJvbHBhZCJdfSwiaWF0IjoxNTIwODUyMzE1LCJpc3MiOiJrYXBwXzEifQ.cGBpxUCLnIQVnRo3EN0TQH_MhcSIsPoRdSYMDzmFZZJMYSVWEH-4dSsit581n-wDLPKi4uIU-Mhx6ssKuXaDhkJNfGOBf3KeHHsaxvHdV6LFY1R_tbyaz98Qg1cQwr1zKq6wTiqklXxqxX5zPhW9vUNPVCXfUbCjyNGAlxEGba5qPx8uWdhkf4_4w8JVnPPzEq9NJYgLFHObx0SJcyZRSoaV1A3RxHd9_ce2GxhTrdCANceSjx0q4ilUqp4DHwlfSuE5bo6KYivxmMzwanTrIRZ8U2f3AHBdjkOnhG1yQytaBgEXX4py6hsltRoTlelMEAGqnuTu7g-ufG_5HPgfVA';
+
+
 
 function sleep(ms) { return new Promise(res => setTimeout(res, ms)); }
 
-const CLIENT_TOKEN = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJjbGllbnQiOnsibmFtZSI6IkthcHRpdm8gTWFuYWdlciBEZXYgbW9kZSIsInJlZGlyZWN0X3VyaSI6Imh0dHA6Ly9sb2NhbGhvc3Q6ODA4Mi9pbmRleC5odG1sIiwic2NvcGUiOlsiY2FwdHVyZSIsImxvY2FsX2NhcHR1cmUiLCJwYWlyIiwiZGlzY292ZXIiLCJyZW1vdGVfY29uZmlnIiwidmlldyIsImxvY2FsX3ZpZXciLCJtb25pdG9yIiwiY29udHJvbHBhZCJdfSwiaWF0IjoxNTIwODUyMzE1LCJpc3MiOiJrYXBwXzEifQ.cGBpxUCLnIQVnRo3EN0TQH_MhcSIsPoRdSYMDzmFZZJMYSVWEH-4dSsit581n-wDLPKi4uIU-Mhx6ssKuXaDhkJNfGOBf3KeHHsaxvHdV6LFY1R_tbyaz98Qg1cQwr1zKq6wTiqklXxqxX5zPhW9vUNPVCXfUbCjyNGAlxEGba5qPx8uWdhkf4_4w8JVnPPzEq9NJYgLFHObx0SJcyZRSoaV1A3RxHd9_ce2GxhTrdCANceSjx0q4ilUqp4DHwlfSuE5bo6KYivxmMzwanTrIRZ8U2f3AHBdjkOnhG1yQytaBgEXX4py6hsltRoTlelMEAGqnuTu7g-ufG_5HPgfVA';
-
 nodeKaptivo.setGlobalClientToken(CLIENT_TOKEN);
-nodeKaptivo.setVerbose(true);
+nodeKaptivo.setVerbose(false);
 
-
-async function testTest()
-{
-  //! -----------------------------------------------------
-  let kap = new nodeKaptivo.NodeKaptivo({kaptivoId: 'HQKZRP'});
-  // let kap = new nodeKaptivo.NodeKaptivo({ip: '198.168.0.27', ssl: true});
-  //
-  let info = (await kap.apiGet({path: '/api/discovery/info'})).result;
-
-  console.log('IP = ' + kap.getKaptivoIp());
-  console.log('FriendlyName = ' + info.friendly_name);
-
-  try {
-    // let accessToken = await kap.authorize({'scope': 'remote_config', admin_name: 'admin', admin_password: 'a'});
-    // let accessToken = await kap.authorize({'scope': 'view', user_name: 'test'});
-    // let accessToken = await kap.authorize({scope: 'pair'});
-    // console.log('accessToken = ' + accessToken);
-  } catch (err) {
-    console.log('CAUGHT AN ERROR');
-    console.log(err);
-  }
-
-  //! -----------------------------------------------------
-  await kap.destroy();
+let g_kapCache = {};
+let g_systemId = localStorage.getItem(SYSTEMID_KEY);
+if (!g_systemId) {
+  g_systemId = require('uuid/v4')();
+  localStorage.setItem(SYSTEMID_KEY, g_systemId);
 }
 
 
+async function getKaptivo(kaptivoId) {
+  let kap = g_kapCache[kaptivoId];
+
+  if (!kap) {
+    try {
+      kap = new nodeKaptivo.NodeKaptivo({kaptivoId});
+      await kap.ping();
+    } catch (e) {
+      throw new Error('Kaptivo not accessible')
+    }
+    g_kapCache[kaptivoId] = kap;
+  }
+
+  return kap;
+}
+
 // initial state
 const state = {
+  kaptivoId: localStorage.getItem(KAPTIVO_ID_KEY) || '',
+  pairingToken: localStorage.getItem(PAIRING_TOKEN_KEY) || '',
 }
 
 // getters
 const getters = {
+  kaptivoId: state => state.kaptivoId,
+  isPaired: state => !!(state.kaptivoId && state.pairingToken),
 }
 
 // actions
 const actions = {
+  async clearRoomPairing ({state, commit}) {
+    console.log('clearRoomPairing is called');
+    commit('setPairing', {kaptivoId: state.kaptivoId, pairingToken: ''});
+  },
   async setupRoomPairing ({state, commit}, {kaptivoId, admin_name, admin_password}) {
     try {
-      let kap = null;
-      let info = null;
-      try {
-        kap = new nodeKaptivo.NodeKaptivo({kaptivoId});
-        info = (await kap.apiGet({path: '/api/discovery/info'})).result;
-        console.log('IP = ' + kap.getKaptivoIp());
-        console.log('FriendlyName = ' + info.friendly_name);
-      } catch (e) {
-        throw new Error('Kaptivo not accessible')
-      }
+      let kap = await getKaptivo(kaptivoId);
 
       let accessToken = null;
       if (admin_name && admin_password) {
         accessToken = await kap.authorize({scope: 'remote_config', admin_name, admin_password});
       }
 
-      let paired_identity = 'PAIREDIDENTITY';
       let pairing_description = 'Kaptivo Manager';
-      let roomPairingToken = await kap.postRoomPairing({accessToken, paired_identity, pairing_description, override:true});
+      let pairingToken = await kap.postRoomPairing({accessToken, paired_identity: g_systemId, pairing_description, override:true});
 
-      return roomPairingToken;
+      commit('setPairing', {kaptivoId, pairingToken});
+      return pairingToken;
     } catch (err) {
-      console.log(JSON.stringify(err));
+      // console.log(JSON.stringify(err));
       if (err.user_message) {
-        throw err.user_message;
+        throw new Error(err.user_message);
       } else {
         throw err;
       }
@@ -81,6 +84,13 @@ const actions = {
 
 // mutations
 const mutations = {
+  setPairing(state, {kaptivoId, pairingToken}) {
+    console.log('setPairing is called');
+    state.kaptivoId = kaptivoId;
+    state.pairingToken = pairingToken;
+    localStorage.setItem(KAPTIVO_ID_KEY, kaptivoId);
+    localStorage.setItem(PAIRING_TOKEN_KEY, pairingToken);
+  },
 }
 
 export default {
