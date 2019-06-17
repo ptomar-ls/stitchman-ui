ac76d2fc584f64b53f54b700d980371f54aa74f5
Nobu Oyama <nobu.oyama@lightblueoptics.com>
2019-06-17 15:04:39 +0100

    Use the published version of node-kaptivo

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store/index.js'
-const nodeKaptivo = require('./@lightblue/node-kaptivo/index.js')
+const nodeKaptivo = require('@lightblue/node-kaptivo')
 
 Vue.config.productionTip = false
 
